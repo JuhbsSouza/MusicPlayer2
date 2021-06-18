a6bd2d1a3ace439ba505394839c6e41404efe04a
Julia <juliasilveira.dsouza@gmail.com>
2021-06-18 07:02:43 -0300

    Initial Setup

---
+++ musicPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  musicPlayer
+//
+//  Created by Julia Silveira de Souza on 18/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
