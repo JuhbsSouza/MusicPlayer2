d4d182ad16edbedaee3dc8d50b83d1101e064606
Julia <juliasilveira.dsouza@gmail.com>
2021-06-24 00:47:46 -0300

    Criado o PlayingMusicView que é a View da playing.

--- musicPlayer/Controllers/PlayingMusicViewController.swift
+++ musicPlayer/Controllers/PlayingMusicViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class PlayingMusicViewController: UIViewController {
-            
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
---
+++ musicPlayer/Views/PlayingMusicView.swift
@@ -0,0 +1,39 @@
+//
+//  PlayingMusicView.swift
+//  musicPlayer
+//
+//  Created by Julia Silveira de Souza on 23/06/21.
+//
+
+import UIKit
+
+class PlayingMusicView: UIView {
+    
+    var musicService = try! MusicService()
+    var music: Music?
+    var collections: [MusicCollection] = []
+    var collectionSelected: MusicCollection?
+    static let identifier = "PlayingMusicView"
+    
+    @IBOutlet weak var albumImage: UIImageView!
+    @IBOutlet weak var musicNameLabel: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var musicProgressBar: UIProgressView!
+    
+    @IBAction func favoriteButton(_ sender: UIButton) {
+        musicService.toggleFavorite(music: self.music!, isFavorite: true)
+    }
+    @IBAction func playAndPauseButton(_ sender: Any) {
+        musicService.startPlaying(music: self.music!)
+    }
+    @IBAction func fowardButton(_ sender: Any) {
+    }
+    @IBAction func backButton(_ sender: Any) {
+    }
+
+    public func configure(item: Music) {
+        musicNameLabel.text = item.title
+        artistNameLabel.text = item.artist
+        self.music = item
+    }
+}
